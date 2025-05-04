8e6c7a7901ff036c19337f1e8854f2a0f5a19d69
Juliette Flobert <Juliette.flobert@edu.ece.fr>
2025-05-04 15:10:58 +0200

    Commit Son

---
+++ Obstacle.h
@@ -0,0 +1,8 @@
+//
+// Created by flobe on 04/05/2025.
+//
+
+#ifndef OBSTACLE_H
+#define OBSTACLE_H
+
+#endif //OBSTACLE_H
---
+++ Son.h
@@ -0,0 +1,8 @@
+//
+// Created by flobe on 04/05/2025.
+//
+
+#ifndef SON_H
+#define SON_H
+
+#endif //SON_H
